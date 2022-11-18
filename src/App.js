f0b2b015e426ea5e385beccbe422adebd99a5658
sava911 <pesss929@gmail.com>
2022-11-19 00:28:43 +0300

    axios, async, json, movies

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json')
+  }
+
+  componentDidMount() {
+    this.getMovies()
+  }
+  render() {
+    const {isLoading} = this.state
+    return <>
+      {isLoading ? 'load...' : 'add'}
+    </>
+  }
+}
+
+export default App;
