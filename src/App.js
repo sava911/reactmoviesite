3652160dcf93790076b435050d4956198c67bf3d
sava911 <pesss929@gmail.com>
2022-11-26 19:51:13 +0300

    finish work site

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
+
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
-  }
+    movies: [],
+  };
+
   getMovies = async () => {
     const {
       data: {
         data: {movies},
       },
-    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
-    this.setState({movies, isLoading: false})
-  }
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({movies, isLoading: false});
+  };
 
   componentDidMount() {
-    this.getMovies()
+    this.getMovies();
   }
+
   render() {
-    const {isLoading, movies} = this.state
-    return <>
-      <div>
-        {isLoading
-          ? "Загрузка..."
-          : movies.map((movie) => {
-            console.log(movie);
-            return (
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Загрузка...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -35,12 +42,14 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
-            );
-          })}
-      </div>
-    </>
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
-export default App;
+export default App
